e4c7088845894b8302a683f0c2a9caff42658ac7
Igor Kantsur <igorkantsur07@gmail.com>
2025-03-25 18:05:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+window.localStorage;
+
+{
+    let scorePushUps = document.getElementById("push-up");
+    localStorage.scorePushUps = +scorePushUps.innerText;
+    function m5push() {
+        if (+scorePushUps.innerText>4) {
+            scorePushUps.innerText = +scorePushUps.innerText - 5;
+            localStorage.scorePushUps = scorePushUps.innerText;    
+        }
+    }
+    function m1push() {
+        if (+scorePushUps.innerText>0) {
+            scorePushUps.innerText = +scorePushUps.innerText - 1;
+            localStorage.scorePushUps = scorePushUps.innerText;    
+        }}
+    function p1push() {
+        scorePushUps.innerText = +scorePushUps.innerText + 1;
+        localStorage.scorePushUps = scorePushUps.innerText;
+    }
+    function p5push() {
+        scorePushUps.innerText = +scorePushUps.innerText + 5;
+        localStorage.scorePushUps = scorePushUps.innerText;
+    }
+}
+
+{
+    let scorePullUps = document.getElementById("pull-up");
+    localStorage.scorePullUps = +scorePullUps.innerText;
+    function m5pull() {
+        if (+scorePullUps.innerText>4) {
+            scorePullUps.innerText = +scorePullUps.innerText - 5;
+            localStorage.scorePullUps = scorePullUps.innerText;    
+        }
+    }
+    function m1pull() {
+        if (+scorePullUps.innerText>0) {
+            scorePullUps.innerText = +scorePullUps.innerText - 1;
+            localStorage.scorePullUps = scorePullUps.innerText;    
+        }}
+    function p1pull() {
+        scorePullUps.innerText = +scorePullUps.innerText + 1;
+        localStorage.scorePullUps = scorePullUps.innerText;
+    }
+    function p5pull() {
+        scorePullUps.innerText = +scorePullUps.innerText + 5;
+        localStorage.scorePullUps = scorePullUps.innerText;
+    }
+}

6d8282d227bb2c4303007234a744a22877a14d36
Igor Kantsur <igorkantsur07@gmail.com>
2025-03-25 19:18:08 +0200

    first commit

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
-import 'https://tomashubelbauer.github.io/github-pages-local-storage/index.js';
+import ('https://tomashubelbauer.github.io/github-pages-local-storage/index.js');
 
-{
-    let scorePushUps = document.getElementById("push-up");
-    localStorage.scorePushUps = +scorePushUps.innerText;
-    function m5push() {
-        if (+scorePushUps.innerText>4) {
-            scorePushUps.innerText = +scorePushUps.innerText - 5;
-            localStorage.scorePushUps = scorePushUps.innerText;    
-        }
-    }
-    function m1push() {
-        if (+scorePushUps.innerText>0) {
-            scorePushUps.innerText = +scorePushUps.innerText - 1;
-            localStorage.scorePushUps = scorePushUps.innerText;    
-        }}
-    function p1push() {
-        scorePushUps.innerText = +scorePushUps.innerText + 1;
-        localStorage.scorePushUps = scorePushUps.innerText;
-    }
-    function p5push() {
-        scorePushUps.innerText = +scorePushUps.innerText + 5;
-        localStorage.scorePushUps = scorePushUps.innerText;
+
+document.getElementById("push-up").innerText = localStorage.scorePushUps;
+let scorePushUps = document.getElementById("push-up");
+localStorage.scorePushUps = +scorePushUps.innerText;
+function m5push() {
+    if (+scorePushUps.innerText>4) {
+        scorePushUps.innerText = +scorePushUps.innerText - 5;
+        localStorage.scorePushUps = scorePushUps.innerText;    
     }
 }
+function m1push() {
+    if (+scorePushUps.innerText>0) {
+        scorePushUps.innerText = +scorePushUps.innerText - 1;
+        localStorage.scorePushUps = scorePushUps.innerText;    
+    }}
+function p1push() {
+    scorePushUps.innerText = +scorePushUps.innerText + 1;
+    localStorage.scorePushUps = scorePushUps.innerText;
+}
+function p5push() {
+    scorePushUps.innerText = +scorePushUps.innerText + 5;
+    localStorage.scorePushUps = scorePushUps.innerText;
+}
 
-{
-    let scorePullUps = document.getElementById("pull-up");
-    localStorage.scorePullUps = +scorePullUps.innerText;
-    function m5pull() {
-        if (+scorePullUps.innerText>4) {
-            scorePullUps.innerText = +scorePullUps.innerText - 5;
-            localStorage.scorePullUps = scorePullUps.innerText;    
-        }
+document.getElementById("pull-up").innerText = localStorage.scorePullUps;
+let scorePullUps = document.getElementById("pull-up");
+localStorage.scorePullUps = +scorePullUps.innerText;
+function m5pull() {
+    if (+scorePullUps.innerText>4) {
+        scorePullUps.innerText = +scorePullUps.innerText - 5;
+        localStorage.scorePullUps = scorePullUps.innerText;    
     }
-    function m1pull() {
-        if (+scorePullUps.innerText>0) {
-            scorePullUps.innerText = +scorePullUps.innerText - 1;
-            localStorage.scorePullUps = scorePullUps.innerText;    
-        }}
-    function p1pull() {
-        scorePullUps.innerText = +scorePullUps.innerText + 1;
-        localStorage.scorePullUps = scorePullUps.innerText;
-    }
-    function p5pull() {
-        scorePullUps.innerText = +scorePullUps.innerText + 5;
-        localStorage.scorePullUps = scorePullUps.innerText;
-    }
+}
+function m1pull() {
+    if (+scorePullUps.innerText>0) {
+        scorePullUps.innerText = +scorePullUps.innerText - 1;
+        localStorage.scorePullUps = scorePullUps.innerText;    
+    }}
+function p1pull() {
+    scorePullUps.innerText = +scorePullUps.innerText + 1;
+    localStorage.scorePullUps = scorePullUps.innerText;
+}
+function p5pull() {
+    scorePullUps.innerText = +scorePullUps.innerText + 5;
+    localStorage.scorePullUps = scorePullUps.innerText;
 }

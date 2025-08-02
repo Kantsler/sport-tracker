1f2a2b6b446fdb3275bc0e0d0642af93255bad98
Igor Kantsur <igorkantsur07@gmail.com>
2025-08-02 23:02:16 +0300

    fix bug

--- scripts/scripti.js
+++ scripts/scripti.js
@@ -468,6 +468,7 @@ const today = new Date().getDate();
         monthRun[today] = 0;
     }
     monthRun[today] = localStorage.scoreRun;
+    monthRun = [];
     let yearRun = JSON.parse(localStorage.getItem("yearRun")) || {};
     yearRun[month] = monthRun.reduce((acc, val) => {
         const num = Number(val);
